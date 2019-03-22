ec05bce8e656b186886180622d21377914039966
adarshrise <adarshrise20@gmail.com>
2019-03-22 20:17:26 +0530

    Push 1

---
+++ app/src/main/java/com/example/featureapptut/tryagain.java
@@ -0,0 +1,21 @@
+package com.example.featureapptut;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class tryagain extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+
+
+    }
+}
